6ec06a3dad6a9d6fd096e9ddda92c8b6d3a45017
fmo01 <fmo01@brueggli.ch>
2025-03-27 14:58:04 +0100

    recreate db migration Terms+Privacy Notice

---
+++ db/data/20250327135352_add_setting_administrative_texts.rb
@@ -0,0 +1,27 @@
+class AddSettingAdministrativeTexts < ActiveRecord::Migration[7.2]
+  def up
+    Setting.find_or_create_by(name: 'TermsText')
+    Setting.find_or_create_by(name: 'PrivacyText')
+
+    SiteSetting.create(setting: Setting.find_by(name: 'TermsText'), value: '[{"type":"paragraph","children":[{"text":""}]}]', provider: 'greenlight') unless SiteSetting.exists?(setting: Setting.find_by(name: 'TermsText'), provider: 'greenlight')
+    SiteSetting.create(setting: Setting.find_by(name: 'PrivacyText'), value: '[{"type":"paragraph","children":[{"text":"Privacy Policy"}]}]', provider: 'greenlight') unless SiteSetting.exists?(setting: Setting.find_by(name: 'PrivacyText'), provider: 'greenlight')
+
+    Tenant.all.each do |tenant|
+      SiteSetting.create(setting: Setting.find_by(name: 'TermsText'), value: '', provider: tenant.name) unless SiteSetting.exists?(setting: Setting.find_by(name: 'TermsText'), provider: tenant.name)
+      SiteSetting.create(setting: Setting.find_by(name: 'PrivacyText'), value: '', provider: tenant.name) unless SiteSetting.exists?(setting: Setting.find_by(name: 'PrivacyText'), provider: tenant.name)
+    end
+  end
+
+  def down
+    Tenant.all.each do |tenant|
+      SiteSetting.find_by(setting: Setting.find_by(name: 'TermsText'), provider: tenant.name).destroy
+      SiteSetting.find_by(setting: Setting.find_by(name: 'PrivacyText'), provider: tenant.name).destroy
+    end
+
+    SiteSetting.find_by(setting: Setting.find_by(name: 'TermsText'), provider: 'greenlight').destroy
+    SiteSetting.find_by(setting: Setting.find_by(name: 'PrivacyText'), provider: 'greenlight').destroy
+
+    Setting.find_by(name: 'TermsText')&.destroy
+    Setting.find_by(name: 'PrivacyText')&.destroy
+  end
+end
