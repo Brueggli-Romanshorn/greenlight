ec54f559159988bad4260cc92e802a166a6c0cbd
fmo1 <fmo01@brueggli.ch>
2025-11-25 11:21:02 +0000

    remove obsolete markdown solution

--- app/javascript/components/legal_informations/Imprint.jsx
+++ app/javascript/components/legal_informations/Imprint.jsx
@@ -15,13 +15,23 @@
 // with Greenlight; if not, see <http://www.gnu.org/licenses/>.
 
 import React from 'react';
-import {  Col, Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import MarkdownViewer from '../shared_components/utilities/MarkdownViewer';
+import RichTextViewer from '../shared_components/utilities/RichTextViewer';
+import useSiteSetting from '../../hooks/queries/site_settings/useSiteSetting';
 import BackButton from '../shared_components/utilities/BackButton';
 
 export default function Imprint() {
   const { t } = useTranslation();
+  const { data: imprint, isLoading: isBoolanLoading } = useSiteSetting(['Imprint']);
+  const { data: imprintContent, isLoading: isLoadingContent } = useSiteSetting(['ImprintText']);
+
+  if (isBoolanLoading) return null;
+  
+  if (imprint === false){
+    return <Navigate to="/404" />;
+  }
+  if(isLoadingContent) return null;
 
   return (
     <>
@@ -34,7 +44,7 @@ export default function Imprint() {
       </Row>
       <Row className="wide-white">
         <Col className="lg-content">
-          <MarkdownViewer fileName="imprint.md" />
+          <RichTextViewer richTextData={ imprintContent } />
         </Col>
       </Row>
       <Row className="py-3">
